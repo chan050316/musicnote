0f7b814de8e71942bd3adb4069c305b3ece07baf
chan-kim <chan.ckeem@gmail.com>
2022-01-24 22:26:58 +0900

    S 2021.1.24.10:26(PM)
    setting IMG and css road

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ app.listen(PORTNUM, () => {
   console.log("start!, I'm listen on port" + PORTNUM);
 });
 
-app.get("/", (req, res) => {
-  res.render(__dirname + "/public/html/main.pug");
-});
+app.use(express.static("public"));
 
 app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render(__dirname + "/public/main.pug");
+  res.sendFile(__dirname + "/IMG/morning.jpeg");
+});
