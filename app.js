f4d139091dded4e6e59d497e98f602b39ce16b14
chan-kim <chan.ckeem@gmail.com>
2022-02-02 23:00:05 +0900

    C 2022.2.2.11:00(PM)
    send data to html

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const { redirect } = require("express/lib/response");
 
 const PORTNUM = "3000";
 
@@ -8,9 +10,20 @@ app.listen(PORTNUM, () => {
 });
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
   res.render(__dirname + "/public/main.pug");
+});
+
+app.post("/create", (req, res) => {
+  const actor = req.body.actor;
+  const song = req.body.song;
+  const time = new Date();
+  const createAt = time.getHours() + time.getMinutes + time.getSeconds;
+  console.log(createAt);
+  // 데이터 베이스에 데이터 추가
+  res.redirect("/");
 });
