a7e4d0e105c126200fad83f5addb6b693426e0bb
chan-kim <chan.ckeem@gmail.com>
2022-02-06 14:21:56 +0900

    F 2022.2.6.2:21(PM)
    setting IMG link

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const { redirect } = require("express/lib/response");
 
 const PORTNUM = "3000";
 
